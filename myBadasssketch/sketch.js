49d134901b871ed75486ca18347a6452b3799ac6
Scott Fitzgerald <scott@droolcup.com>
2020-10-14 17:27:18 -0400

    adding in some rad stuff

---
+++ myBadasssketch/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  // put setup code here
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  // put drawing code here
+    textAlign(CENTER);
+    textSize(32);
+    text('Booyah', width/2, height/2);
+    noLoop();
+}
